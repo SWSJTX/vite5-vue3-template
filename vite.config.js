8865d027089f399d8b25be9242512b7cb5506598
SWSJTX <1311836549@qq.com>
2024-01-08 09:39:52 +0800

    build: pack to remove console

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import { visualizer } from 'rollup-plugin-visualizer'
 import Components from 'unplugin-vue-components/vite'
 import { defineConfig, loadEnv } from 'vite'
 import viteCompression from 'vite-plugin-compression'
+import viteRemoveConsole from 'vite-plugin-remove-console'
 import vueSetupExtend from 'vite-plugin-vue-setup-extend'
 
 const root = process.cwd()
@@ -35,7 +36,8 @@ export default defineConfig(({ mode }) => {
       // 打包资源分析
       visualizer({
         open: false
-      })
+      }),
+      viteRemoveConsole()
     ],
     resolve: {
       alias: {
@@ -53,6 +55,10 @@ export default defineConfig(({ mode }) => {
         }
       }
     },
+    // esbuild 去除 console
+    // esbuild: {
+    //   drop: ['console', 'debugger']
+    // },
     build: {
       rollupOptions: {
         output: {
@@ -67,6 +73,15 @@ export default defineConfig(({ mode }) => {
           }
         }
       }
+      // 需要 pnpm add terser -D
+      // minify: 'terser',
+      // terser 去除console
+      // terserOptions: {
+      //   compress: {
+      //     drop_console: true,
+      //     drop_debugger: true
+      //   }
+      // }
     }
   }
 })
